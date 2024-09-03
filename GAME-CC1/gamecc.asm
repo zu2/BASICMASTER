63de5f074abcb2173c9e627b68ad656804ea253b
zu2 <3023910+zu2@users.noreply.github.com>
2024-09-03 09:52:29 +0900

    refactoring

--- GAME-CC1/gamecc.asm
+++ GAME-CC1/gamecc.asm
@@ -41,129 +41,17 @@ CURPOS		EQU     $FFF5
 LINBUF		EQU		$0500
 LBUFSIZ		EQU		6+1			; 数値入力しかしないので '-'と5桁まで。末尾に00
 *
-			IF		0
-*
-*	AccAB+IX => IX
-*
-ADX			STX		ADXWK
-			ADDB	ADXWK+1
-			ADCA	ADXWK
-			STAB	ADXWK+1
-			STAA	ADXWK
-			LDX		ADXWK
-			RTS
-*
-*	AccAB*2+IX => OX
-*
-ADX2		STX		ADXWK
-			ASLB
-			ROLA
-			ADDB	ADXWK+1
-			ADCA	ADXWK
-			STAB	ADXWK+1
-			STAA	ADXWK
-			LDX		ADXWK
-			RTS
-			ENDIF
-			IF	0
-*
-*	MULTIPLY	AB = AB * (IX,IX+1)
-*
-MULTIPLY	STAB    W68+1
-			STAA    W68
-			BEQ		MULTI0
-			LDAB	1,X
-			LDAA	0,X
-			STAB	W66+1
-			STAA	W66
-MULTI		LDX		#16
-			CLRA
-			CLRB
-ML1			LSR     W68
-			ROR     W68+1
-			BCC     ML2
-			ADDB    W66+1
-			ADCA    W66
-ML2			ASL     W66+1
-			ROL     W66
-			DEX
-			BNE     ML1
-			RTS
-*
-*	MULTIPLY	AB = B * (IX,IX+1)
-*	when AccA==0
-*
-MULTI0		LDAB	1,X
-			STAB	W66+1
-			LDAB	0,X
-			STAB	W66
-			LDX		#8
-			CLRB
-ML01		LSR     W68+1
-			BCC     ML02
-			ADDB    W66+1
-			ADCA    W66
-ML02		ASL     W66+1
-			ROL     W66
-			DEX
-			BNE     ML01
-			RTS
-			ENDIF
-			IF		0
-*
-*	MULTIPLY	AB = (IX,IX+1)*AB
-*
-MULTIPLY	STX		TDXWK
-			LDX		0,X				;   6
-			STX		W66				;	4
-			STAB	W68+1			;	4
-			STAA	W68				;	4
-			BEQ		MULTI02			;	4
-MULTI		LDX		#16             ;   3
-			CLRA					;   2
-			CLRB					;   2
-			FCB		$8C				;	3			SKIP2
-ML01	    ASLB					;   2	1
-			ROLA					;   2	1
-			ROL     W68+1			;   6	3
-			ROL     W68				;   6	3
-			BCC     ML02			;   4	2
-			ADDB	W66+1			;   3	2
-			ADCA	W66             ;   3	2
-ML02		DEX						;	4	1		30cycle/loop => *16=>480cycle
-			BNE     ML01            ;   4
-			LDX		TDXWK
-			RTS
-*
-*	MULTIPLY	AB = (W68+1) * (W66,W66+1)	when AccA==0
-*
-MULTI02		LDX		#8				;	3		before 3+2+2=7
-			CLRA					;	2
-			CLRB					;	2
-ML201	    ASLB					;	2		loop 24cycle/loop -> 192cycle
-			ROLA					;	2
-			ROL     W68+1			;	6
-			BCC     ML202			;	4
-			ADDB	W66+1			;	3
-			ADCA	W66             ;	3
-ML202		DEX						;	4
-			BNE     ML201           ;	4
-			LDX		TDXWK
-			RTS
-			ENDIF
-			IF		1
-*
 *	MULTIPLY Dr Jefyll's method
+*			cf. http://forum.6502.org/viewtopic.php?p=19958#p19958
 *	MULTIPLY	AB = (IX,IX+1)*AB
 *
-MULTIPLY	STX		TDXWK			;   5 2
-			LDX		0,X				;   6 2
+MULTIPLY	LDX		0,X				;   6 2		呼び出し元でW66に入れてから呼びたいが難しい
 			STX		W66				;	4 2
-			STAA	W68				;	4 2
-			CLRA					;   2 1
 			STAB	W68+1			;	4 2
-;			BEQ		ML03			;	4 2			Is W68,W68+1 a multiple of 256?
 			CLRB					;   2 1
+			STAA	W68				;	4 2
+			BEQ		MULTI02			;   4 2
+			CLRA					;   2 1
 			LDX		#8              ;   3 3
 ML01	    ROR		W68+1			;   6 3
 			BCC		ML02			;	4 2
@@ -190,10 +78,23 @@ ML05		LSRA					;   2 1
 			ROR		W68				;   6 3
 ML06		LDAA	W68				;	3 2
 ML07		LDAB	W68+1			;	3 2
-			LDX		TDXWK			;   4 2
 			RTS						;	5 1
-			ENDIF
-
+*
+*	MULTIPLY	AB = (W68+1) * (W66,W66+1)	when AccA==0
+*
+MULTI02		LDX		#8				;	3		before 3+2+2=7
+;			CLRA					;	2		ここに来る時はAccAB=0
+;			CLRB					;	2
+ML201	    ASLB					;	2		loop 24cycle/loop -> 192cycle
+			ROLA					;	2
+			ROL     W68+1			;	6
+			BCC     ML202			;	4
+			ADDB	W66+1			;	3
+			ADCA	W66             ;	3
+ML202		DEX						;	4
+			BNE     ML201           ;	4
+			RTS
+*
 *
 *	Division by powers of 2
 *			AccAB		Number to be divided
@@ -206,14 +107,14 @@ DIVPOW2		TSX
 			NEGA			; If the dividend is negative, make it positive
 			NEGB
 			SBCA	#0
-DIVPOW01	PSHB
-			PSHA
+DIVPOW01	STAB	IXSAVE+1	; PSH/PULよりもDPが速い 4+4+4+4 vs 4+4+3+3
+			STAA	IXSAVE
 			ANDB	3,X		; mask
 			ANDA	2,X		; mask
 			STAB	_MOD+1
 			STAA	_MOD
-			PULA
-			PULB
+			LDAB	IXSAVE+1
+			LDAA	IXSAVE
 DIVPOW02	ASRA
 			RORB
 			DEC		4,X
@@ -234,80 +135,97 @@ DIVZERO		STX		_MOD
 			CLRA
 			RTS
 *
-*	AB<=(SP+4,SP+5)/(SP+2,SP+3), _MOD<=modulo
+*	AB<=(SP+2,SP+3)/AB, _MOD<=modulo
 *  (DIVPOS:W68<=W68/W66  AB<=Modulo)
 *
-DIVIDE		TSX
-			LDX		2,X			; If the divisor is 0, an error occurs.
-			BEQ		ERROR
-			IF		1			; If the dividend is 0, the answer is 0
-			TSX
-			LDX		4,X
+DIVIDE		LDX		0,X			; If the dividend is 0, the answer is 0
 			BEQ		DIVZERO
-			ENDIF
-			TSX
-			CLR		W82+5
-			LDAB	3,X
-			LDAA	2,X
-			BPL		DIVIDE02
-			INC     W82+5
+			STX		W68
+			STAA	W82+5		; sign. bit7=0:plus, other:minus
+			BMI		DIVIDE01	; AccA<0 -> AccAB<0
+			BNE		DIVIDE02	; AccA>0
+			TSTB
+			BNE		DIVIDE02
+			BRA		ERROR		; AccAB=0 -> Error
+DIVIDE01	COM     W82+5		; AccAB<0
 			NEGA
 			NEGB
 			SBCA	#0
 DIVIDE02	STAB	W66+1
 			STAA	W66
-			LDAB	5,X
-			LDAA	4,X
+			LDAA	W68
 			BPL     DIVIDE03
 			DEC     W82+5
-			NEGA
-			NEGB
-			SBCA	#0
-DIVIDE03	STAB	W68+1
-			STAA	W68
-			JSR     DIVPOS
+			LDAB	W68+1		; 3 2
+			NEGA				; 2 1
+			NEGB				; 2 1
+			SBCA	#0			; 2 2
+			STAB	W68+1		; 4 2
+			STAA	W68			; 4 2
+DIVIDE03	JSR     DIVPOS
 			STAB    W4E+1		; modulo
 			STAA    W4E
 			LDAB    W68+1
 			LDAA    W68
 			TST     W82+5
-			BEQ     DIVIDE99
+			BPL     DIVIDE99
 			NEGA
 			NEGB
 			SBCA	#0
 DIVIDE99	RTS
 *
-*	DIVIDE Positive number
-*			W68 = W68/W66 AB=MODULO
+*	DIVIDE Positive number / non restoring division algorithm
+*			W68 = W68/W66 AB=MODULO (W68=Q,W66=M,N=16,A=AccAB)
+*			cf. https://www.javatpoint.com/non-restoring-division-algorithm-for-unsigned-integer
+*			cf. https://www.wizforest.com/tech/Z80vs6502/div.html;p2
 *			IX is destroyed
 *
-DIVPOS		LDB		W68			; 3 3	the dividend less than or equal to 255 (<=255?)
+DIVPOS		EQU		*
+			LDB		W68			; 3 3	the dividend less than or equal to 255 (<=255?)
 			BNE		DIVPOS00	; 4 2		↑7cycle overhead
 			LDA		W68+1		; 3 2	W68 = W68<<8
 			STAA	W68			; 3 2
 			STAB	W68+1		; 3 2	clear W68+1
-			LDX		#8			; 3 3
+			LDX		#8-1		; 3 3
 			BRA		DIVPOS10	; 4 2	AccB is 0, so the jump destination is CLRA.
-DIVPOS00	LDX		#16			; 3 3
-			CLRB				; 2 1
+DIVPOS00	LDX		#16-1		; 3 3
+			CLRB				; 2 1	clear AB
 DIVPOS10	CLRA				; 2 1	7cycle for preparation
-DIVPOS01	ASL		W68+1		; 6 3
+DIVPOS01	ROL		W68+1		; 6 3
 			ROL		W68			; 6 3
-			ROLB				; 2 1
+			ROLB				; 2 1	shift AB<<1
 			ROLA				; 2 1
 			SUBB	W66+1		; 3 2
 			SBCA	W66			; 3 2
-			BCS		DIVPOS02	; 4 2	↑26cycle
-			INC		W68+1		; 6 3
+			BCS		DIVMNS00	; 4 2
+			SEC					; 2 1
+			ROL		W68+1		; 6 3
+			ROL		W68			; 6 3	;
+DIVPLS01	ROLB				; 2 1
+			ROLA				; 2 1
+			SUBB	W66+1		; 3 2
+			SBCA	W66			; 3 2
+			BCS		DIVMNS02	; 4 2	↑14cycle
+DIVPLS02	SEC					; 2 1
+DIVPLS03	ROL		W68+1		; 6 3
+			ROL		W68			; 6 3
 			DEX					; 4 1
-			BNE		DIVPOS01	; 4 2	+14cycle => 40cycle/loop => 640cycle for 16bit repetitions
-			RTS					; 5 1	5cycle
-DIVPOS02	ADDB	W66+1		; 3 2
+			BNE		DIVPLS01	; 4 2	↑22cycle	/ 36cycle for plus
+			RTS					; 5 1
+DIVMNS00	ASL		W68+1		; 6 3
+			ROL		W68			; 6 3	↑12cycle
+DIVMNS01	ROLB				; 2 1
+			ROLA				; 2 1
+			ADDB	W66+1		; 3 2
 			ADCA	W66			; 3 2
+			BCS		DIVPLS03	; 4 2	↑14cycle
+DIVMNS02	ASL		W68+1		; 6 3
+			ROL		W68			; 6 3
 			DEX					; 4 1
-			BNE		DIVPOS01	; 4 2	+14cycle => 40cycle/loop => 640cycle for 16bit repetitions
-			RTS					; 5 1	5cycle
-			ENDIF
+			BNE		DIVMNS01	; 4 2	↑20cycle	/ 34cycle for minus
+			ADDB	W66+1		; 3 2
+			ADCA	W66			; 3 2
+			RTS					; 5 1
 *
 *	RANDOM 1 between AB
 *			cf.https://tinyework.flston.com/xorshift-on-hd6301
@@ -341,31 +259,27 @@ RANDOM		STAB	W6A+1
 * PRINT 4 HEX AccAB
 PRHEX4		PSHB
 			TAB
-			BSR     PR2HEX
+			BSR		PRHEX2
 			PULB
 * PRINT 2 HEX AccB
 PRHEX2		TBA
-PR2HEX		BSR     PRHEXH
-			TAB
-			ANDB    #$F
-			BRA     PRHEXL
-* PRINT 1 HEX AccB high nibble
-PRHEXH		LSRB
-			LSRB
-			LSRB
-			LSRB
-* PRINT 1 HEX AccB low nibble
-PRHEXL		CMPB    #$A
-			BMI     PH1
-			ADDB    #7
-PH1			ADDB	#$30
-			PSHA
+PR2HEX	    BSR		PRHEXH
 			TBA
-			JSR		ASCOUT
-			PULA
-			RTS
+			ANDA    #$F
+			BRA     PRHEXL
+* PRINT 1 HEX AccA high nibble
+PRHEXH		LSRA
+			LSRA
+			LSRA
+			LSRA
+* PRINT 1 HEX AccA low nibble
+PRHEXL		ADDA	#$90		; 2 2
+			DAA					; 2 1
+			ADCA	#$40		; 2 2
+PH1			DAA					; 2 1	8cycle/6bytes
+			JMP		ASCOUT
 *
-* PRINT AccB of SPACE 
+* Print a space AccB times
 *
 PRINTTAB	LDAA	#' '
 			TSTB
@@ -377,17 +291,17 @@ TABE		RTS
 *
 *	PRINT AccAB decimal left
 *
-PRINTL		CMPA	#0
+PRINTL		TSTA	#0
 			BNE		PRINTL1
 			CMPB	#10
 			BCS		PR1DEC
 PRINTL1		BSR     BDSGN
-PRINTSTR	LDAA    0,X
-			INX
-			TSTA
-			BEQ		TABE
-	        JSR     ASCOUT
-			BRA     PRINTSTR
+PRINTSTR	LDAA    0,X				; 文字列が$00で始まってないこと(空で無いこと）
+PRINTL2		JSR     ASCOUT
+PRINTL3		INX
+			LDAA	0,X
+			BNE		PRINTL2
+			RTS
 *
 *		1桁の数値(0-9)は計算するまでもない
 *
@@ -395,18 +309,14 @@ PR1DEC		ADDB	#'0
 			TBA
 			JMP		ASCOUT
 *
-*	PRINT (SP+2,SP+3) decimal right AccB
+*	PRINT AccAB decimal right width=WPRINTR
 *
-PRINTR		STAB	W88			; 指定した桁数
-			TSX
-			LDAA	2,X
-			LDAB	3,X
-			BSR		BDSGN
+PRINTR		BSR		BDSGN
 			STX		W64			; 
-			LDAB	W88			; 右詰め桁数
+			LDAB	WPRINTR		; 右詰め桁数
 			ADDB	W64+1		; 表示する最初の桁のアドレス下位
 			SUBB	#W82+6		; バッファ最終桁+1のアドレス下位
-			BMI		PR1
+			BMI		PRINTSTR
 			BSR		PRINTTAB
 PR1			BRA		PRINTSTR
 *
@@ -414,8 +324,7 @@ PR1			BRA		PRINTSTR
 *	AccAB => (IX)
 *
 BDSGN		TSTA
-			BMI		BDNEG
-			BRA		BYNDEC
+			BPL		BYNDEC
 BDNEG		NEGA
 			NEGB
 			SBCA	#0
@@ -424,18 +333,14 @@ BDNEG		NEGA
 			DEX
 			STAB	0,X
 PRE			RTS
-OUTNUM		PSHB
-			PSHA
-			LDAB	#5
-			BSR		PRINTR
-			RTS
 *
 *	AccAB to Decimal W82+1..W82+5
 *			return IX decimal top (zero blanking)
 *
-BYNDEC		LDX		#W82+1			; 10進文字保存領域。W82+1〜W82+5
+BYNDEC		EQU		*
+			LDX		#W82+1			; 10進文字保存領域。W82+1〜W82+5
 			STX		W6C
-			LDX		#CTABLE
+BYND10000	LDX		#CTABLE
 BYNDEC0		STX		W6E
 			CLR		W68				; 商=0
 BYNDEC2		INC		W68				; 商を+1
@@ -468,13 +373,12 @@ ZB1			INX						; Zero blanking
 			BEQ		ZB1
 			COM		W82+5
 ZB2			RTS
-CTABLE		FDB     10000
-			FDB     1000
-			FDB     100
+CTABLE		FDB     10000			; $2710
+			FDB     1000			; $03E8
+			FDB     100				; $0064
 			FDB     10
 ;			FDB		1
 CTABLEE		EQU		*
-			ENDIF
 *
 BEL			EQU		$07
 BS			EQU		$08
@@ -578,26 +482,40 @@ DIV0ERROR	FCB	$0D
 *
 *	Workarea
 *
-			IF		0
-ADXWK		EQU		$0061
-			ENDIF
-TDXWK		EQU		$0063
-IXSAVE		RMB		2
-DBWK		RMB		2
+_TMP0		EQU		$00A3
+_TMP1		EQU		$00A5
+_TMP2		EQU		$00A7
+_TMP3		EQU		$00A9
+_TMP4		EQU		$00B1
+_TMP5		EQU		$00B3
+IXSAVE		EQU		$00B5		; Temporary IX WORK ; ADXWKと一緒でもいいか？
+*
+DBWK		RMB		2			; INPUT/DECBIN WORK
 W4E			RMB		2
 _MOD		EQU		W4E
 W52			EQU		$00E0
 RND			EQU		W52
-W64			RMB		2
-;W66			RMB		2
-;W68			RMB		2
+W64			RMB		2			; PRINTR WORK
+;W66		RMB		2
+;W68		RMB		2
 W66			EQU		$005D
 W68			EQU		$006F
-W6A			RMB		2
-W6C			RMB		2
-W6E			RMB		2
-W70			RMB		2
-W82			RMB		6
+W6A			RMB		2			; RANDOM WORK
+W6C			RMB		2			; BYNDEC WORK
+W6E			RMB		2			; BYNDEC WORK
+W82			RMB		6			; DIVIDE BINDEC/WORK
 			FCB		0
-W88			RMB		1
+W88			RMB		1			;
+WPRINTR		EQU		*			; PRINTR WORK 
+_TMP6		RMB		2
+_TMP7		RMB		2
+_TMP8		RMB		2
+_TMP9		RMB		2
+_TMP10		RMB		2
+_TMP11		RMB		2
+_TMP12		RMB		2
+_TMP13		RMB		2
+_TMP14		RMB		2
+_TMP15		RMB		2
 PRGEND		EQU		*
+* vim:set ts=4:
